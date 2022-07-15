fa3e34be1298fdd99de03bc962f947d9beeb0947
alvator92 <alvator@list.ru>
2022-07-15 18:54:42 +0300

    add application.yml add logback.xml add pom.xml dependencies update StartClass with CommandLineRunner

--- src/main/java/webclient/StartClass.java
+++ src/main/java/webclient/StartClass.java
@@ -1,13 +1,44 @@
 package webclient;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import webclient.backend.configuration.db.hibernate.HibernateConfig;
+
+import java.util.Iterator;
+import java.util.Map;
 
 @SpringBootApplication
-public class StartClass {
+public class StartClass implements CommandLineRunner {
+    @Autowired
+    private JpaProperties jpaProperties;
+
+    @Autowired
+    private HibernateConfig hibernateConfig;
     public static void main(String[] args) {
+
         SpringApplication.run(StartClass.class, args);
         System.out.println("Hello world");
 
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("DataSource : " + hibernateConfig);
+
+        Iterator hmIterator = jpaProperties.getProperties().entrySet().iterator();
+
+        while (hmIterator.hasNext()) {
+
+            Map.Entry mapElement
+                    = (Map.Entry)hmIterator.next();
+            int marks = ((int)mapElement.getValue() + 10);
+
+            // Printing mark corresponding to string entries
+            System.out.println(mapElement.getKey() + " : "
+                    + marks);
+        }
     }
 }
