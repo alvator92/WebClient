45c5de268ddb8e8069c404ba89afc4e2312eb02b
alvator92 <alvator@list.ru>
2022-07-18 12:44:34 +0300

    add Liquibase and test Tables

---
+++ src/test/java/webclient/backend/common/CommonTest.java
@@ -0,0 +1,32 @@
+package webclient.backend.common;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommonTest {
+
+    @Test
+    void getDataTimeZone() {
+    }
+
+    @Test
+    void getDateTime() {
+    }
+
+    @Test
+    void getDate() {
+    }
+
+    @Test
+    void getRandomNumber() {
+    }
+
+    @Test
+    void getRandomNumberMount() {
+    }
+
+    @Test
+    void getRandomString() {
+    }
+}
