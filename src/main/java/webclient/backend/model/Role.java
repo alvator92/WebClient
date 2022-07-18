abe7bf524b0efd42a34bdb1e9ca00eaf91662464
alvator92 <alvator@list.ru>
2022-07-19 00:21:52 +0300

    add User, Permission, Role, users.csv update Liquibase

---
+++ src/main/java/webclient/backend/model/Role.java
@@ -0,0 +1,21 @@
+package webclient.backend.model;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import java.util.Set;
+import java.util.stream.Collectors;
+public enum Role {
+
+  USER(Set.of(Permission.USER)),
+  MODERATOR(Set.of(Permission.USER, Permission.MODERATE));
+
+  private final Set<Permission> permissions;
+
+  Role(Set<Permission> permissions) {
+    this.permissions = permissions;
+  }
+
+  public Set<Permission> getPermissions() {
+    return permissions;
+  }
+
+}
